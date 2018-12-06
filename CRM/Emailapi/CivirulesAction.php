687aca57c743cbef4219bab234240b7f8ea5f4b0
Jaap Jansma <jaapjansma@users.noreply.github.com>
2018-12-06 17:31:22 +0100

    Create CivirulesAction.php

---
+++ CRM/Emailapi/CivirulesAction.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * @author Jaap Jansma <jaap.jansma@civicoop.org>
+ * This class exists for backwards compatibility.
+ * This is needed because the original class name could still be available in the queue.
+ */
+ 
+ class CRM_Emailapi_CivirulesAction extends CRM_Emailapi_CivirulesAction_Send {
+ }
